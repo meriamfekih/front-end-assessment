3c29b80133f67419ed06371684ad65fa04834dce
meriamfekih <107309893+meriamfekih@users.noreply.github.com>
2023-09-25 00:29:59 +0200

    feat: display quotes

---
+++ Task2/script.js
@@ -0,0 +1,33 @@
+const quotesContainer = document.getElementById("quote-container");
+const displayQuoteBtn = document.getElementById("display-quote");
+
+// Create Quote html element and add it to the container
+const addQuoteToContainer = ({ content, author }) => {
+  const quoteElement = `<div class="quote-card">
+    <p>${content}</p>
+    <div class="author-name">${author}</div>
+  </div>`;
+  quotesContainer.insertAdjacentHTML("beforeend", quoteElement);
+};
+
+// Fetch n quotes from api
+const fetchQuotes = async (totalQuotes) => {
+  try {
+    const responses = await Promise.all(
+      Array(totalQuotes)
+        .fill(1)
+        .map(() => fetch("https://api.quotable.io/random"))
+    );
+    return await Promise.all(responses.map((response) => response.json()));
+  } catch (e) {
+    console.debug("Failed to fetch quotes");
+  }
+  return [];
+};
+
+displayQuoteBtn.addEventListener("click", () => {
+  const quotes = fetchQuotes(3);
+  // reset container before adding quotes
+  quotesContainer.innerHTML = "";
+  quotes.forEach(addQuoteToContainer);
+});
